5c00c6558f91e2c75b2cac412c7f06b3ba4c053a
doni sanjaya <donis293@gmail.com>
2022-04-24 11:27:05 +0700

    cointable finish

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
+// import {  } from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
+// import { createMuiTheme } from "@material-ui/core";
 import {
 	Container,
 	createTheme,
@@ -29,8 +31,8 @@ export default function CoinsTable() {
 	const [coins, setCoins] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [search, setSearch] = useState("");
-
-	const { currency } = CryptoState();
+	const [page, setPage] = useState(1);
+	const { currency, symbol } = CryptoState();
 
 	// const useStyles = makeStyles({
 	// 	row: {
@@ -41,15 +43,9 @@ export default function CoinsTable() {
 	// 		},
 	// 		fontFamily: "Montserrat",
 	// 	},
-	// 	pagination: {
-	// 		"& .MuiPaginationItem-root": {
-	// 			color: "gold",
-	// 		},
-	// 	},
 	// });
-
 	// const classes = useStyles();
-	// const history = useHistory();
+	const history = useHistory();
 
 	const darkTheme = createTheme({
 		palette: {
@@ -73,7 +69,17 @@ export default function CoinsTable() {
 		fetchCoins();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [currency]);
-
+	const handleSearch = () => {
+		return coins.filter(
+			(coin) =>
+				coin.name
+					.toLowerCase()
+					.includes(search) ||
+				coin.symbol
+					.toLowerCase()
+					.includes(search)
+		);
+	};
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<Container style={{ textAlign: "center" }}>
@@ -116,12 +122,212 @@ export default function CoinsTable() {
 									backgroundColor:
 										"#EEBC1D",
 								}}
-							></TableHead>
+							>
+								<TableRow>
+									{[
+										"Coin",
+										"Price",
+										"24h Change",
+										"Market Cap",
+									].map(
+										(
+											head
+										) => (
+											<TableCell
+												style={{
+													color: "black",
+													fontWeight: "700",
+													fontFamily: "Montserrat",
+												}}
+												key={
+													head
+												}
+												align={
+													head ===
+													"Coin"
+														? "left"
+														: "right"
+												}
+											>
+												{
+													head
+												}
+											</TableCell>
+										)
+									)}
+								</TableRow>
+							</TableHead>
+
+							<TableBody>
+								{handleSearch()
+									.slice(
+										(page -
+											1) *
+											10,
+										(page -
+											1) *
+											10 +
+											10
+									)
+									.map(
+										(
+											row
+										) => {
+											const profit =
+												row.price_change_percentage_24h >
+												0;
+											return (
+												<TableRow
+													onClick={() =>
+														history.push(
+															`/coins/${row.id}`
+														)
+													}
+													// className={
+													// 	classes.row
+													// }
+													style={{
+														backgroundColor:
+															"#16171a",
+														cursor: "pointer",
+														"&:hover": {
+															backgroundColor:
+																"#131111",
+														},
+														fontFamily: "Montserrat",
+													}}
+													key={
+														row.name
+													}
+												>
+													<TableCell
+														component="th"
+														scope="row"
+														style={{
+															display: "flex",
+															gap: 15,
+														}}
+													>
+														{" "}
+														<img
+															src={
+																row?.image
+															}
+															alt={
+																row.name
+															}
+															height="50"
+															style={{
+																marginBottom: 10,
+															}}
+														/>
+														<div
+															style={{
+																display: "flex",
+																flexDirection:
+																	"column",
+															}}
+														>
+															<span
+																style={{
+																	textTransform:
+																		"uppercase",
+																	fontSize: 22,
+																}}
+															>
+																{
+																	row.symbol
+																}
+															</span>
+															<span
+																style={{
+																	color: "darkgrey",
+																}}
+															>
+																{
+																	row.name
+																}
+															</span>
+														</div>
+													</TableCell>
+													<TableCell align="right">
+														{
+															symbol
+														}{" "}
+														{numberWithCommas(
+															row.current_price.toFixed(
+																2
+															)
+														)}
+													</TableCell>
+													<TableCell
+														align="right"
+														style={{
+															color:
+																profit >
+																0
+																	? "rgb(14, 203, 129)"
+																	: "red",
+															fontWeight: 500,
+														}}
+													>
+														{profit &&
+															"+"}
+														{row.price_change_percentage_24h.toFixed(
+															2
+														)}
+
+														%
+													</TableCell>
+													<TableCell align="right">
+														{
+															symbol
+														}{" "}
+														{numberWithCommas(
+															row.market_cap
+																.toString()
+																.slice(
+																	0,
+																	-6
+																)
+														)}
+
+														M
+													</TableCell>
+												</TableRow>
+											);
+										}
+									)}
+							</TableBody>
 						</Table>
 					)}
 				</TableContainer>
 
-				{/* Comes from @material-ui/lab */}
+				<Pagination
+					count={(
+						handleSearch()
+							?.length /
+						10
+					).toFixed(0)}
+					style={{
+						padding: 20,
+						width: "100%",
+						display: "flex",
+						justifyContent:
+							"center",
+						// "& .MuiPaginationItem-root":
+						// 	{
+						color: "gold",
+						// },
+					}}
+					onChange={(_, value) => {
+						setPage(value);
+						window.scroll(
+							0,
+							450
+						);
+					}}
+				/>
 			</Container>
 		</ThemeProvider>
 	);
